eca69426bcddde461e8e2c3b0846535b69925e48
Saswat Mohanty <saswat.mohanty@in.tesco.com>
2018-03-06 15:32:19 +0530

    added coucbase in config files

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using DataAccessLib;
 
 namespace ProductServiceApi
 {
@@ -24,6 +25,8 @@ namespace ProductServiceApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddTransient<IProductDataAccess, ProductDataAccess>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
